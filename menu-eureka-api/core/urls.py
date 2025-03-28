e2f7f1fb41dd2fae917c222cbb93baa5f1e225f0
ColellaG <colellag.dev@gmail.com>
2025-03-28 18:16:15 -0300

    Ajustando configuración de healthcheck para Railway

--- menu-eureka-api/core/urls.py
+++ menu-eureka-api/core/urls.py
@@ -34,9 +34,9 @@ router.register(r'categories', CategoryViewSet)
 router.register(r'products', ProductViewSet)
 
 urlpatterns = [
-    path('', health_check, name='health_check'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', health_check, name='health_check'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
